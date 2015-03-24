d01bbb6cbcc1dd3ad35d7713eb1ec978ddeccfdc
izumix03 <takahiro.izumikawa@gmail.com>
2015-03-24 11:35:56 +0000

    add paging process

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit]
+  before_action :set_article, only: [:show]
 
   # GET /articles
   # GET /articles.json
@@ -12,8 +12,47 @@ class ArticlesController < ApplicationController
   # GET /articles/1.json
   def show
   end
+  
+  def previousPage
+    previousId = getPrevious(params[:id])
+    redirect_to :action => "show", :id => previousId
+  end
+  
+  def nextPage
+    nextId = getNext(params[:id])
+    redirect_to :action => "show", :id => nextId
+  end
 
   private
+    def getPrevious(id)
+      articleId = id.to_i
+      while articleId >= 1 do
+        articleId -=1
+        begin
+          if Article.find(articleId) != nil
+            return articleId
+          end
+        rescue
+        end
+      end
+      return id
+    end
+    
+    def getNext(id)
+      articleId = id.to_i
+      while articleId < Article.maximum('id') do
+        articleId +=1
+        begin
+          tempArticle = Article.find(articleId)
+          if tempArticle != nil
+            return articleId
+          end
+        rescue
+        end
+      end
+      return id
+    end
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
