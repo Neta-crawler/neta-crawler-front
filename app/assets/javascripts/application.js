f4488e5a51de2ff6153245439d5d6044a680d7f5
izumix03 <takahiro.izumikawa@gmail.com>
2015-03-24 08:24:05 +0000

    infinite scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require jquery.infinitescroll
 //= require turbolinks
 //= require_tree .
+  $("#articles .page").infinitescroll({
+      loading: {
+      img:     "http://www.mytreedb.com/uploads/mytreedb/loader/ajax_loader_blue_48.gif",
+      msgText: "ロード中..."
+      },
+    navSelector: "nav.pagination" ,
+    nextSelector: "nav.pagination a[rel=next]" ,
+    itemSelector: "#articles tr.article" 
+  })
+    
